b3e2073615ca424edb4e76bf27570f54f5e5c91c
Lautaro Cozzani <lautaromail@gmail.com>
2019-06-13 15:44:15 -0300

    initial testing

---
+++ src/inversify-agenda.spec.ts
@@ -0,0 +1,80 @@
+import 'reflect-metadata';
+import { defineTaskService, AgendaTaskCommand, AgendaTask, inversifyAgendaTasksConfiguration } from './inversify-agenda';
+import { Container } from 'inversify';
+import * as Agenda from 'agenda';
+
+describe('defineTaskService', () => {
+
+    class TestCommand implements AgendaTaskCommand {
+        async execute() {
+            console.log('DO NOTHING');
+        }
+    }
+
+    it('should define agenda task with target command injected with inversify', () => {
+        const container: Container = {
+            get<T>(serviceIdentifier: any) {
+                return new TestCommand();
+            }
+        } as Container;
+        let func: (job: Agenda.Job<any>, done: (err?: Error) => void) => void;
+        const agenda: Agenda = {
+            define<T>(name: string, handler: (job: Agenda.Job<T>, done: (err?: Error) => void) => void) {
+                console.log('MOCK');
+                func = handler;
+            }
+        } as Agenda;
+        spyOn(agenda, 'define').and.callThrough();
+        spyOn(container, 'get').and.callThrough();
+        defineTaskService(
+            container,
+            agenda,
+            'test',
+            TestCommand as any
+        );
+        expect(agenda.define).toHaveBeenCalledWith('test', jasmine.any(Function));
+        func.call(this, {}, (err?: Error) => console.log('DONE', err));
+        expect(container.get).toHaveBeenCalledWith(TestCommand);
+    })
+});
+
+describe('AgendaTask', () => {
+    
+    @AgendaTask('test', '10 minutes')
+    class TestCommand implements AgendaTaskCommand {
+        async execute() {
+            console.log('DO NOTHING');
+        }
+    }
+
+    @AgendaTask('test2', '20 minutes')
+    class TestCommand2 implements AgendaTaskCommand {
+        async execute() {
+            console.log('DO NOTHING');
+        }
+    }
+
+    @AgendaTask('test22', '20 minutes')
+    class TestCommand22 implements AgendaTaskCommand {
+        async execute() {
+            console.log('DO NOTHING');
+        }
+    }
+
+    it('should invoke decorate class with @inject() add task definition and interval definition', () => {
+        expect(inversifyAgendaTasksConfiguration.tasks.find(t => t.key === 'test')).toEqual({
+            key: 'test',
+            target: TestCommand
+        });
+        expect(inversifyAgendaTasksConfiguration.tasks.find(t => t.key === 'test2')).toEqual({
+            key: 'test2',
+            target: TestCommand2
+        });
+        expect(inversifyAgendaTasksConfiguration.tasks.find(t => t.key === 'test22')).toEqual({
+            key: 'test22',
+            target: TestCommand22
+        });
+        expect(inversifyAgendaTasksConfiguration.intervals['10 minutes']).toEqual(['test']);
+        expect(inversifyAgendaTasksConfiguration.intervals['20 minutes']).toEqual(['test2', 'test22']);
+    });
+});
--- src/inversify-agenda.ts
+++ src/inversify-agenda.ts
@@ -2,83 +2,95 @@ import * as Agenda from 'agenda';
 import { Container, decorate, injectable } from 'inversify';
 
 export interface AgendaTaskConfig {
-  key: string;
-  target: any;
+    key: string;
+    target: any;
 }
 
 export interface AgendaTaskCommand {
-  execute(job: Agenda.Job<Agenda.JobAttributesData>): Promise<void>;
+    execute(job: Agenda.Job<Agenda.JobAttributesData>): Promise<void>;
 }
 
-const tasksDefinitions: AgendaTaskConfig[] = [];
-const intervalsDefinitions: { [key: string]: string[]; } = {};
+export class InversifyAgendaTasksConfiguration {
+
+    tasks: AgendaTaskConfig[] = [];
+    intervals: { [key: string]: string[]; } = {};
+
+    decorateAndRegister(target: any, key: string, ...int: (number | string)[]) {
+        decorate(injectable(), target);
+        this.tasks.push({ key, target });
+        int.forEach(i => {
+            this.intervals[i] = this.intervals[i] || [];
+            this.intervals[i].push(key);
+        });
+    }
+}
+
+export const inversifyAgendaTasksConfiguration = new InversifyAgendaTasksConfiguration();
 
 export function AgendaTask(key: string, ...int: (number | string)[]) {
-  return (target: any) => {
-    decorate(injectable(), target);
-    tasksDefinitions.push({ key, target });
-    int.forEach(i => {
-      intervalsDefinitions[i] = intervalsDefinitions[i] || [];
-      intervalsDefinitions[i].push(key);
-    });
-  };
+    return (target: any) => {
+        inversifyAgendaTasksConfiguration.decorateAndRegister(target, key, ...int);
+    };
 }
 
 export function defineTaskService(
-  container: Container,
-  agenda: Agenda,
-  key: string,
-  target: symbol
+    container: Container,
+    agenda: Agenda,
+    key: string,
+    target: symbol
 ) {
-  agenda.define(key, async (job: Agenda.Job<Agenda.JobAttributesData>, done: (err?: Error) => void) => {
-    try {
-      await (container.get(target) as any).execute(job);
-      done();
-    } catch (err) {
-      done(err);
-    }
-  });
+    agenda.define(key, async (job: Agenda.Job<Agenda.JobAttributesData>, done: (err?: Error) => void) => {
+        try {
+            await (container.get(target) as any).execute(job);
+            done();
+        } catch (err) {
+            done(err);
+        }
+    });
 }
 
 export class InversifyAgenda {
 
-  constructor(
-    private container: Container,
-    private config: {
-      agenda?: Agenda,
-      db?: {
-        address: string,
-        collection?: string,
-        options?: any
-      }
-    }
-  ) {
-    if (this.config.agenda && this.config.db) {
-      throw new Error('Cannot use agenda instance and db configuration at the same time');
-    }
-    if (!this.config.agenda && !this.config.db) {
-      throw new Error('Yu have to configure agenda or db connection');
-    }
-    if (this.config.db) {
-      this.config.agenda = new Agenda({
-        db: {
-          address: this.config.db.address,
-          collection: this.config.db.collection,
-          options: this.config.db.options
+    constructor(
+        private container: Container,
+        private config: {
+            agenda?: Agenda,
+            db?: {
+                address: string,
+                collection?: string,
+                options?: any
+            }
         }
-      });
+    ) {
+        if (this.config.agenda && this.config.db) {
+            throw new Error('Cannot use agenda instance and db configuration at the same time');
+        }
+        if (!this.config.agenda && !this.config.db) {
+            throw new Error('Yu have to configure agenda or db connection');
+        }
+        if (this.config.db) {
+            this.config.agenda = new Agenda({
+                db: {
+                    address: this.config.db.address,
+                    collection: this.config.db.collection,
+                    options: this.config.db.options
+                }
+            });
+        }
     }
-  }
 
-  build() {
-    tasksDefinitions.forEach(task => {
-      this.container.bind(task.target).toSelf();
-      defineTaskService(this.container, this.config.agenda, task.key, task.target);
-    });
+    build() {
+        inversifyAgendaTasksConfiguration.tasks.forEach(task => {
+            this.container.bind(task.target).toSelf();
+            defineTaskService(this.container, this.config.agenda, task.key, task.target);
+        });
 
-    this.config.agenda.on('ready', () =>
-      Object.keys(intervalsDefinitions).map(interval => this.config.agenda.every(interval, intervalsDefinitions[interval]))
-    );
-    return this.config.agenda;
-  }
+        this.config.agenda.on('ready', () =>
+            Object.keys(inversifyAgendaTasksConfiguration.intervals)
+                .map(interval => 
+                    this.config.agenda.every(interval, inversifyAgendaTasksConfiguration.intervals[interval])
+                )
+        );
+        return this.config.agenda;
+    }
 }
